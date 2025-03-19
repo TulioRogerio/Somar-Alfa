81df7bdac16d7522d1e0cde39ce25ed85e59e4bf
TulioRogerio <tulio.rogerio@educador.edu.es.gov.br>
2025-03-19 12:20:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Obtém a URL base do site
-    let basePath = window.location.origin + "/";
+    let basePath = window.location.origin + "/Somar-Alfa/";
 
     // Inserindo o menu diretamente via JavaScript
     document.getElementById("menu-container").innerHTML = `
@@ -10,64 +10,64 @@ document.addEventListener("DOMContentLoaded", function () {
         <li>
             <a href="#" class="menu-item">Gestão de Usuários <span class="arrow">▼</span></a>
             <ul class="submenu">
-                 à <li><a href="${basePath}/Somar-Alfa/gestao-usuarios/cadastro.html">Cadastro</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/gestao-usuarios/ativacao-inativacao.html">Ativação e Inativação</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/gestao-usuarios/movimentacao.html">Movimentação</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/gestao-usuarios/funcoes.html">Funções</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/gestao-usuarios/log-atividades.html">Log de atividades</a></li>
+                 <li><a href="${basePath}/gestao-usuarios/cadastro.html">Cadastro</a></li>
+                 <li><a href="${basePath}/gestao-usuarios/ativacao-inativacao.html">Ativação e Inativação</a></li>
+                 <li><a href="${basePath}/gestao-usuarios/movimentacao.html">Movimentação</a></li>
+                 <li><a href="${basePath}/gestao-usuarios/funcoes.html">Funções</a></li>
+                 <li><a href="${basePath}/gestao-usuarios/log-atividades.html">Log de atividades</a></li>
             </ul>
         </li>
 
         <li>
             <a href="#" class="menu-item">Plano de Ação e Monitoramento <span class="arrow">▼</span></a>
             <ul class="submenu">
-                 à <li><a href="${basePath}/Somar-Alfa/plano-acao/diagnostico.html">Diagnóstico</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/plano-acao/problemas-desafios.html">Problemas e Desafios</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/plano-acao/mapa-acao.html">Mapa de Ação</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/plano-acao/teste-consistencia.html">Teste de Consistência</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/plano-acao/dashboard-progresso.html">Dashboard de Progresso</a></li>
+                 <li><a href="${basePath}/plano-acao/diagnostico.html">Diagnóstico</a></li>
+                 <li><a href="${basePath}/plano-acao/problemas-desafios.html">Problemas e Desafios</a></li>
+                 <li><a href="${basePath}/plano-acao/mapa-acao.html">Mapa de Ação</a></li>
+                 <li><a href="${basePath}/plano-acao/teste-consistencia.html">Teste de Consistência</a></li>
+                 <li><a href="${basePath}/plano-acao/dashboard-progresso.html">Dashboard de Progresso</a></li>
             </ul>
         </li>
 
         <li>
             <a href="#" class="menu-item">Visitas Técnicas <span class="arrow">▼</span></a>
             <ul class="submenu">
-                 à <li><a href="${basePath}/Somar-Alfa/visitas-tecnicas/calendario.html">Calendário</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/visitas-tecnicas/agendamento.html">Agendamento</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/visitas-tecnicas/linha-tempo.html">Linha do Tempo</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/visitas-tecnicas/modelos-atas.html">Modelos de Atas</a></li>
+                 <li><a href="${basePath}/visitas-tecnicas/calendario.html">Calendário</a></li>
+                 <li><a href="${basePath}/visitas-tecnicas/agendamento.html">Agendamento</a></li>
+                 <li><a href="${basePath}/visitas-tecnicas/linha-tempo.html">Linha do Tempo</a></li>
+                 <li><a href="${basePath}/visitas-tecnicas/modelos-atas.html">Modelos de Atas</a></li>
             </ul>
         </li>
 
         <li>
             <a href="#" class="menu-item">Comunicação <span class="arrow">▼</span></a>
             <ul class="submenu">
-                 à <li><a href="${basePath}/Somar-Alfa/comunicacao/caixa-entrada.html">Caixa de Entrada</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/comunicacao/envio-mensagens.html">Envio de mensagens</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/comunicacao/relatorios-comunicacao.html">Relatórios de Comunicação</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/comunicacao/tutoriais.html">Tutoriais</a></li>
+                 <li><a href="${basePath}/comunicacao/caixa-entrada.html">Caixa de Entrada</a></li>
+                 <li><a href="${basePath}/comunicacao/envio-mensagens.html">Envio de mensagens</a></li>
+                 <li><a href="${basePath}/comunicacao/relatorios-comunicacao.html">Relatórios de Comunicação</a></li>
+                 <li><a href="${basePath}/comunicacao/tutoriais.html">Tutoriais</a></li>
             </ul>
         </li>
 
         <li>
             <a href="#" class="menu-item">Painel de Controle e BI <span class="arrow">▼</span></a>
             <ul class="submenu">
-                 à <li><a href="${basePath}/Somar-Alfa/painel-controle/dashboards.html">Dashboards</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/painel-controle/indicadores-estruturantes.html">Indicadores Estruturantes</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/painel-controle/indicadores-execucao.html">Indicadores de Execução</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/painel-controle/resultados-avaliacoes.html">Resultados: Avaliações Externas</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/painel-controle/exportar-dados.html">Exportar dados</a></li>
+                 <li><a href="${basePath}/painel-controle/dashboards.html">Dashboards</a></li>
+                 <li><a href="${basePath}/painel-controle/indicadores-estruturantes.html">Indicadores Estruturantes</a></li>
+                 <li><a href="${basePath}/painel-controle/indicadores-execucao.html">Indicadores de Execução</a></li>
+                 <li><a href="${basePath}/painel-controle/resultados-avaliacoes.html">Resultados: Avaliações Externas</a></li>
+                 <li><a href="${basePath}/painel-controle/exportar-dados.html">Exportar dados</a></li>
             </ul>
         </li>
 
         <li>
             <a href="#" class="menu-item">Diagnóstico e Gestão de Dados <span class="arrow">▼</span></a>
             <ul class="submenu">
-                 à <li><a href="${basePath}/Somar-Alfa/diagnostico-gestao-dados/matriculas-abandono-aprovacao.html">Matrículas, abandono, aprovação</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/diagnostico-gestao-dados/avaliacoes-externas.html">Avaliações Externas</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/diagnostico-gestao-dados/distorcao-idade-serie.html">Distorção Idade-Série</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/diagnostico-gestao-dados/questoes-etnico-raciais.html">Questões Étnico-Raciais</a></li>
-                 à <li><a href="${basePath}/Somar-Alfa/diagnostico-gestao-dados/importar-exportar-dados.html">Importar/Exportar dados</a></li>
+                 <li><a href="${basePath}/diagnostico-gestao-dados/matriculas-abandono-aprovacao.html">Matrículas, abandono, aprovação</a></li>
+                 <li><a href="${basePath}/diagnostico-gestao-dados/avaliacoes-externas.html">Avaliações Externas</a></li>
+                 <li><a href="${basePath}/diagnostico-gestao-dados/distorcao-idade-serie.html">Distorção Idade-Série</a></li>
+                 <li><a href="${basePath}/diagnostico-gestao-dados/questoes-etnico-raciais.html">Questões Étnico-Raciais</a></li>
+                 <li><a href="${basePath}/diagnostico-gestao-dados/importar-exportar-dados.html">Importar/Exportar dados</a></li>
             </ul>
         </li>
 
@@ -76,21 +76,21 @@ document.addEventListener("DOMContentLoaded", function () {
             <ul class="submenu">
                 <li><a href="#">Indicadores Estruturantes<span class="arrow">▼</span></a>
                     <ul class="submenu">
-                         à <li><a href="${basePath}/Somar-Alfa/indicadores-relatorios/aulas-frequencia.html">Aulas Dadas e Frequência</a></li>
-                         à <li><a href="${basePath}/Somar-Alfa/indicadores-relatorios/proficiencia.html">Proficiência</a></li>
+                         <li><a href="${basePath}/indicadores-relatorios/aulas-frequencia.html">Aulas Dadas e Frequência</a></li>
+                         <li><a href="${basePath}/indicadores-relatorios/proficiencia.html">Proficiência</a></li>
                     </ul>
                 </li>
                 <li><a href="#">Indicadores de Execução<span class="arrow">▼</span></a></li>
                     <ul class="submenu">
-                         à <li><a href="${basePath}/Somar-Alfa/indicadores-relatorios/atas.html">Atas</a></li>
-                         à <li><a href="${basePath}/Somar-Alfa/indicadores-relatorios/tarefas.html">Tarefas</a></li>
-                         à <li><a href="${basePath}/Somar-Alfa/indicadores-relatorios/produtos.html">Produtos</a></li>                      
-                         à <li><a href="${basePath}/Somar-Alfa/indicadores-relatorios/planos-acao.html">Planos de Ação</a></li>
-                         à <li><a href="${basePath}/Somar-Alfa/indicadores-relatorios/mapas-acao.html">Mapas de Ação</a></li>
-                         à <li><a href="${basePath}/Somar-Alfa/indicadores-relatorios/teste-consistencia.html">Teste de Consistência</a></li>                      
+                         <li><a href="${basePath}/indicadores-relatorios/atas.html">Atas</a></li>
+                         <li><a href="${basePath}/indicadores-relatorios/tarefas.html">Tarefas</a></li>
+                         <li><a href="${basePath}/indicadores-relatorios/produtos.html">Produtos</a></li>                      
+                         <li><a href="${basePath}/indicadores-relatorios/planos-acao.html">Planos de Ação</a></li>
+                         <li><a href="${basePath}/indicadores-relatorios/mapas-acao.html">Mapas de Ação</a></li>
+                         <li><a href="${basePath}/indicadores-relatorios/teste-consistencia.html">Teste de Consistência</a></li>                      
                     </ul>
               
-                 à <li><a href="${basePath}/Somar-Alfa/indicadores-relatorios/exportar-relatorios.html">Exportar Relatórios</a></li>
+                 <li><a href="${basePath}/indicadores-relatorios/exportar-relatorios.html">Exportar Relatórios</a></li>
             </ul>
         </li>
     </ul>
