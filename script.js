d448e8651c406b84229b670b2729e7fb98c598b2
TulioRogerio <tulio.rogerio@educador.edu.es.gov.br>
2025-03-20 11:39:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     <h2>Somar Alfa 2</h2>
     <ul class="menu">
 <li>
-     <li href="${basePath}/index.html" class="menu-item">Início</li>                        
+     <a href="${basePath}/index.html" class="menu-item">Início</a>                        
             <a href="#" class="menu-item">Gestão de Usuários <span class="arrow">▼</span></a>
             <ul class="submenu">
                  <li><a href="${basePath}/gestao-usuarios/cadastro.html">Cadastro</a></li>
